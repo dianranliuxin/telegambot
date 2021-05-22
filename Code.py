81027dc5c97ef49e68be226b94e13a2624a24d84
流星 <84608890+dianranliuxin@users.noreply.github.com>
2021-05-22 16:38:47 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,13 +1,8 @@
-1
 pip install python-telegram-bot --upgrade
 import telegram<font></font>
-2
 <font></font>
-3
 # 替换为实际的 token<font></font>
-4
 bot = telegram.Bot(token='012345678:xxxxxxxxxxxxxxxxx-xxxxxxxxxx-xxxxxx')
-1
 bot.send_message(chat_id='@XXXXXX', text="新消息")
 bot.send_message(chat_id='@XXXXXX', text='<a href="http://slowread.net/monitor-hostloc/">HOSTLOC 交易贴提醒</a>.', parse_mode=telegram.ParseMode.HTML)
 bot.send_message(chat_id='@XXXXXX', text='<a href="'%20+%20href%20+%20'">' + title + '</a>.', parse_mode=telegram.ParseMode.HTML, disable_web_page_preview=True)
